66eebfb373a515bddca2b259fe22c53bcc69089f
Ann-Annita23 <mainaann43241@gmail.com>
2026-04-16 12:24:45 +0300

    First commit

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Write your code here!
+function displayPost(posts) {
+    const postList = document.getElementById("post-list");
+    if (!postList) return;
+    postList.innerHTML = "";
+
+    posts.forEach((post) => {
+        const li = document.createElement("li");
+        const h1 = document.createElement("h1");
+        const p = document.createElement("p");
+
+        h1.textContent = post.title;
+        p.textContent = post.body;
+
+        li.appendChild(h1);
+        li.appendChild(p);
+        postList.appendChild(li);
+    });
+}
+
+async function fetchPosts() {
+    try{
+        //Apply await to the fetch call and assign the result to a variable called response
+const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+
+//Parse the JSON data
+const posts = await response.json()
+
+// call displayPosts() after the fetched
+displayPost(posts)
+} catch (error) {
+    console.error('Error fetching posts:', error);
+}
+}
+
+//Initiliaze the fetch process
+getPosts()
+    
+   
